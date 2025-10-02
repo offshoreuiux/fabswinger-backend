4ca9eb70ee372bcef871a8e1dcc9c52511c5a558
offshoreuiux <jaskarangrover9@hmail.com>
2025-10-02 17:07:26 +0530

    signup fixed

--- utils/transporter.js
+++ utils/transporter.js
@@ -29,14 +29,23 @@ dotenv.config();
 //   },
 // });
 
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   //   host:process.env.SMTP_HOST,
+//   //   port: process.env.SMTP_PORT,
+//   //   secure: false,
+//   auth: {
+//     user: process.env.SMTP_USER,
+//     pass: process.env.SMTP_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  //   host:process.env.SMTP_HOST,
-  //   port: process.env.SMTP_PORT,
-  //   secure: false,
+  host: process.env.MAILTRAP_HOST,
+  port: process.env.MAILTRAP_PORT,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.MAILTRAP_USER,
+    pass: process.env.MAILTRAP_PASS,
   },
 });
 
